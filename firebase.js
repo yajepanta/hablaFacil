47076e4caf0cd96866c75375b4b20d8d8559768a
LauraJimenezB <laura271260@gmail.com>
2021-01-25 17:57:06 -0500

    firebase/router

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAnB6qeNWrDEZCD3myHa92WkkMS1FilWG8",
+    authDomain: "hablafacilapp-20c5a.firebaseapp.com",
+    projectId: "hablafacilapp-20c5a",
+    storageBucket: "hablafacilapp-20c5a.appspot.com",
+    messagingSenderId: "1062852859493",
+    appId: "1:1062852859493:web:192d737e54054dbfa8188c",
+    measurementId: "G-X84WQ1NF7C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,5 +22,59 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/voto">Voto responsable</Link>
+            </li>
+            <li>
+              <Link to="/miembrodemesa">Miembro de mesa</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/voto">
+            <Voto />
+          </Route>
+          <Route path="/miembrodemesa">
+            <MiembroDeMesa />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Voto() {
+  return <h2>Voto responsable 2021</h2>;
+}
+
+function MiembroDeMesa() {
+  return <h2>¿Eres miembro de mesa?</h2>;
+}
