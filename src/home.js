7513943f9bc91d2a356b78db63a55fe9ad713414
LauraJimenezB <laura271260@gmail.com>
2021-01-25 21:46:29 -0500

    get data from firebase

--- src/home.js
+++ src/home.js
@@ -1,3 +1,38 @@
+import { useState, useEffect, React } from 'react';
+import firebase from './firebase';
+
 export function Home() {
-    return <h2>Home</h2>;
+
+  const [slides, setSlides] = useState([]);
+
+    useEffect(() => {
+        let mounted = true;
+        firebase.firestore().collection('articulos').where('categoria','==','home').orderBy('order', 'asc').onSnapshot((snapshot)=>{
+          if (mounted) {
+          const slides = snapshot.docs.map((doc)=> ({
+            id: doc.id,
+            ...doc.data()
+          }))
+          setSlides(slides);
+        }
+        })
+        return function cleanup() {
+          mounted = false;
+        }
+        }, [])
+
+    const listSlides = slides.map((slide)=> 
+    <div key={slide.id} className='slide'>
+        <div className='tituloSlide'>{slide.titulo}</div>
+        <div className='subtituloSlide'>{slide.subtitulo}</div>
+        <div className='contenidoSlide'>{slide.contenido}</div>
+        <button>Ver más</button>
+    </div>
+    )
+    return (
+      <main>
+        <h2>Home</h2>
+        <section>{listSlides}</section>
+      </main>
+    );
   }
--- src/miembroMesa.js
+++ src/miembroMesa.js
@@ -1,4 +1,37 @@
+import { useState, useEffect, React } from 'react';
+import firebase from './firebase';
+
 export function MiembroDeMesa() {
-    return <h2>¿Eres miembro de mesa?</h2>;
+
+  const [slides, setSlides] = useState([]);
+    useEffect(() => {
+        let mounted = true;
+        firebase.firestore().collection('articulos').where('categoria','==','mesa').orderBy('order', 'asc').onSnapshot((snapshot)=>{
+          if (mounted) {
+          const slides = snapshot.docs.map((doc)=> ({
+            id: doc.id,
+            ...doc.data()
+          }))
+          setSlides(slides);
+        }
+        })
+        return function cleanup() {
+          mounted = false;
+        }
+        }, [])
+
+    const listSlides = slides.map((slide)=> 
+    <div key={slide.id} className='slide'>
+        <div className='tituloSlide'>{slide.titulo}</div>
+        <div className='subtituloSlide'>{slide.subtitulo}</div>
+        <div className='contenidoSlide'>{slide.contenido}</div>
+    </div>
+    )
+    return (
+      <main>
+        <h2>¿Eres miembro de mesa?</h2>
+        <section>{listSlides}</section>
+      </main>
+    );
   }
   
