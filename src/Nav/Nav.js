6108d825cedcd64ec1bbbf2fb649d1c335b653fc
YajePanta <yajepanta@gmail.com>
2021-01-25 22:22:04 -0500

    componente nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <input type="checkbox" id="check-menu" />
+            <label for="check-menu"> <img src="./img/menu.png" alt="menu-burguer" /></label>
+        </nav>
+    )
+}
+
+export default Nav;
