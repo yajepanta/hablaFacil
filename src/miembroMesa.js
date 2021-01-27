eeb97f1f1786daa52ec7d8fe65f08a475c5b5f78
LauraJimenezB <laura271260@gmail.com>
2021-01-27 15:04:02 -0500

    comentarios mesa.js

--- src/miembroMesa.js
+++ src/miembroMesa.js
@@ -35,6 +35,16 @@ export function MiembroDeMesa(props) {
           <Contador />
           <Share/>
         </section>
+        <section className="comentario">
+      <p className="titleComentario">¿Te sirvió la publicación? Dejanos tu comentario</p>
+        <div
+          className="fb-comments"
+          data-href="https://developers.facebook.com/docs/plugins/comments#configurator"
+          data-width="150"
+          data-numposts="4"
+          data-colorscheme="dark"
+        ></div>
+      </section>
       </main>
     );
   }
