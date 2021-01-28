63be7b06fd1085742a18c2241dfd287cae82a90a
LauraJimenezB <laura271260@gmail.com>
2021-01-27 20:30:56 -0500

    last css details

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -24,7 +24,7 @@ const Nav = (searchField, setSearchField) => {
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <a className="nav-link" href="/hablaFacil">
+              <a className="nav-link" href="/#">
                 Home <span className="sr-only">(current)</span>
               </a>
             </li>
